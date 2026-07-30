4ac2826258385f4bd845ded06d081eff1409cd6d
Aurélien Bulliard <aurelien.bulliard@edu.hefr.ch>
2026-07-30 22:42:35 +0200

    separated connections.go

---
+++ cff/cmd/connections.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2025 Aurélien Bulliard
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+
+func fetchConnections(from, to, date, heure string) (Connections, error){
+	var url string
+	switch {
+	case heure != "" && date != "":
+		url = fmt.Sprintf("http://transport.opendata.ch/v1/connections?from=%s&to=%s&date=%s&time=%s", from, to, date, heure)
+	case heure != "" && date == "":
+		url = fmt.Sprintf("http://transport.opendata.ch/v1/connections?from=%s&to=%s&time=%s", from, to, heure)
+	default:
+		url = fmt.Sprintf("http://transport.opendata.ch/v1/connections?from=%s&to=%s", from, to)
+	}
+
+	//Query API
+	resp, err := http.Get(url)
+	if err != nil {
+		return Connections{}, err
+	}
+	defer resp.Body.Close()
+	//Read response:
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Connections{}, err
+	}
+	//Parse
+	var conn Connections
+	errJson := json.Unmarshal(body, &conn)
+	if errJson != nil {
+		return Connections{}, errJson
+	}
+	return conn, nil
+}
--- cff/cmd/trip.go
+++ cff/cmd/trip.go
@@ -5,47 +5,14 @@ package cmd
 
 import (
 	_ "embed"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
 )
 
-func fetchConnections(from, to, date, heure string) (Connections, error){
-	var url string
-	switch {
-	case heure != "" && date != "":
-		url = fmt.Sprintf("http://transport.opendata.ch/v1/connections?from=%s&to=%s&date=%s&time=%s", from, to, date, heure)
-	case heure != "" && date == "":
-		url = fmt.Sprintf("http://transport.opendata.ch/v1/connections?from=%s&to=%s&time=%s", from, to, heure)
-	default:
-		url = fmt.Sprintf("http://transport.opendata.ch/v1/connections?from=%s&to=%s", from, to)
-	}
-
-	//Query API
-	resp, err := http.Get(url)
-	if err != nil {
-		return Connections{}, err
-	}
-	defer resp.Body.Close()
-	//Read response:
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return Connections{}, err
-	}
-	//Parse
-	var conn Connections
-	errJson := json.Unmarshal(body, &conn)
-	if errJson != nil {
-		return Connections{}, errJson
-	}
-	return conn, nil
-}
 
 func ftrip(cmd *cobra.Command, args []string) {
 	date, err := cmd.Flags().GetString("date")
